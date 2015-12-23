0dd0dc548da551ca443d3d5d0a93b0d3a24750c1
Ryan Jackson <r_j@hotmail.co.uk>
2015-12-23 00:22:48 +0000

    Part 2 of day 6

--- day06.hs
+++ day06.hs
@@ -1,17 +1,31 @@
 
 import System.IO (readFile)
+import Data.List (foldl')
 import Data.Set (union, (\\), fromList, empty, size)
 import Data.List.Split (splitOn)
+import Data.Maybe (fromMaybe, Maybe(Just))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-area ([x1, y1], [x2, y2]) = fromList [[x, y] | x <- [x1..x2], y <- [y1..y2]]
+area ([x1, y1], [x2, y2]) = [[x, y] | x <- [x1..x2], y <- [y1..y2]]
+
+areaSet = fromList . area
 
 toggle old new = union (old \\ new) (new \\ old)
 
 --
 
-applyCmd on (cmd, pos) = cmd on $ area pos
+applyCmd on ((combineCmd, _), pos) = combineCmd on $ areaSet pos
 
 numLightsOn = size . foldl applyCmd empty
+
+--
+
+updateLight u m xy = Map.alter (Just . u . fromMaybe 0) xy m
+
+applyCmd' curr ((_, updateCmd), pos) = foldl' (updateLight updateCmd) curr $ area pos
+
+totalBrightness = Map.fold (+) 0 . foldl applyCmd' Map.empty
 
 --
 
@@ -20,9 +34,9 @@ coords = map read . splitOn ","
 
 buildCmd c (from:"through":to:[]) = (c, (coords from, coords to))
 
-parseLine ("turn":"on":rest) = buildCmd union $ rest
-parseLine ("turn":"off":rest) = buildCmd (\\) rest
-parseLine ("toggle":rest) = buildCmd toggle $ rest
+parseLine ("turn":"on":rest) = buildCmd (union, (+1)) rest
+parseLine ("turn":"off":rest) = buildCmd ((\\), max 0 . (subtract 1)) rest
+parseLine ("toggle":rest) = buildCmd (toggle, (+2)) rest
 
 parse = map (parseLine . words) . lines
 
@@ -30,3 +44,4 @@ main = do
 	input <- readFile "input06.txt"
 	let parsed = parse input
 	putStrLn $ show $ numLightsOn $ parsed
+	putStrLn $ show $ totalBrightness $ parsed
